248755ecc4d7869944d33fbf2e6069748daff566
D-Bednarczyk <d.k.bednarczyk@gmail.com>
2023-04-15 12:02:46 +0200

    video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,38 @@
+//const Eliframe = document.querySelector('#vimeo-player');
+import throttle from 'lodash.throttle';
 
+const player = new Vimeo.Player('vimeo-player');
+
+console.log(
+  JSON.parse(localStorage.getItem('videoplayer-current-time')).seconds
+);
+
+player.on(
+  'timeupdate',
+  throttle(data => {
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
+  }, 1000)
+);
+
+/* player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+}); */
+
+player
+  .setCurrentTime(
+    JSON.parse(localStorage.getItem('videoplayer-current-time')).seconds
+  )
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
