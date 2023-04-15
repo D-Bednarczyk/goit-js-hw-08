b335720fed22a0d2c8fdc8a03301f020c55c70f1
D-Bednarczyk <d.k.bednarczyk@gmail.com>
2023-04-15 12:26:09 +0200

    form

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+import throttle from 'lodash.throttle';
 
+const ElForm = document.querySelector('.feedback-form');
+/* const ElEmail = document.querySelector('input');
+const ElMessage = document.querySelector('textarea');
+const ElButton = document.querySelector('button'); */
+
+//console.log(JSON.parse(localStorage.getItem('feedback-form-state')).email);
+
+ElForm.addEventListener('input', throttle(handleInput, 500));
+ElForm.addEventListener('submit', handleSubmit);
+
+function handleInput(event) {
+  const {
+    elements: { email, message },
+  } = event.currentTarget;
+  localStorage.setItem(
+    'feedback-form-state',
+    JSON.stringify({ email: email.value, message: message.value })
+  );
+}
+
+ElForm.elements.email.value =
+  JSON.parse(localStorage.getItem('feedback-form-state')).email || '';
+ElForm.elements.message.value =
+  JSON.parse(localStorage.getItem('feedback-form-state')).message || '';
+
+function handleSubmit(event) {
+  event.preventDefault();
+  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+  localStorage.removeItem('feedback-form-state');
+  event.currentTarget.reset();
+}
